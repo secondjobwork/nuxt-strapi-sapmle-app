6d0d9469fcb78296878b99679a8172f543815fec
secondjobwork <52063554+secondjobwork@users.noreply.github.com>
2019-06-23 18:06:57 +0900

    Hello (#1)
    Componentを作ってnuxtjsをgenerateした時の'nuxt-strapi-sapmle-app'をHello
    Componentを使って出力させる

---
+++ components/Hello.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1 class="title">{{ message }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'nuxt-strapi-sapmle-app'
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
